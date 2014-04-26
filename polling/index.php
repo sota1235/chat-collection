288568086383cf79ed96b1ddad6092dc2c4ab855
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-27 01:56:48 +0900

    add polling/index.php / view作成

---
+++ polling/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once("./get_comments");
+$comments = get_comments();
+?>
+<html>
+  <head>
+    <title>Polling Chat</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="script/script.js">
+  <head>
+  <body>
+    <div id="wrapper">
+      <form action="/submit" method="POST">
+        <input type="text" name="txt">
+        <input type="submit" value="発言">
+      </form>
+      <div id="comment">
+        <?php $comments ?>
+      </div>
+    </div>
+  </body>
+</html>
