2657594d7a791d5ee8f28164cdb38df8dcf016a7
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-27 02:31:06 +0900

    fix index.php / bug fix

--- polling/index.php
+++ polling/index.php
@@ -1,22 +1,23 @@
 <?php
 require_once("./getter.php");
 
-$h = new Getter;
-$comments = $h -> get_comments();
+$h = new Getter();
 
 if(isset($_POST["comment"])) {
   $h -> write_comments($_POST["comment"]);
 }
+
+$comments = $h -> get_comments();
 ?>
 <html>
   <head>
     <title>Polling Chat</title>
     <link rel="stylesheet" href="css/style.css">
-    <script src="script/script.js">
+    <script src="script/script.js"></script>
   <head>
   <body>
     <div id="wrapper">
-      <form action="/index.php" method="POST">
+      <form action="index.php" method="POST">
         <input type="text" name="comment">
         <input type="submit" value="発言">
       </form>
