28a9cf7d1e725b5cde9d98974244bf709001d9fe
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-27 16:06:37 +0900

    fix handle.rb / json返せた

--- polling/models/handle.rb
+++ polling/models/handle.rb
@@ -5,6 +5,7 @@ class Handler
   def getter
     f = open("./models/comments.txt", "r")
     res = Array.new
+    res2 = Hash.new
     f.each {|line|
       tmp = line.split("::")
       obj = Hash.new
@@ -13,7 +14,8 @@ class Handler
       res.push(obj)
     }
     f.close
-    return res.to_json
+    res2['line'] = res
+    return res2.to_json
   end
 
   def writer(comment)
