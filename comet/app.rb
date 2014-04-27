4da619a2b3035494cea2ad7119511e4475ab16d1
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-28 02:52:10 +0900

    fix app.rb

--- comet/app.rb
+++ comet/app.rb
@@ -8,25 +8,21 @@ require 'cgi'
 require_relative 'models/init'
 
 class Server < Sinatra::Base
-  @flat = true
-
   get '/' do
+    @flag = true
     haml :index
   end
 
   # comments要素を返す
   get '/comments' do
     handle = Handler.new
-    comment = nil
     loop do
       if @flag then
-        comment = handle.getter
+        json handle.getter
         @flag = false
         break
       end
-      sleep 1
     end
-    json comment
   end
 
   post '/post' do
