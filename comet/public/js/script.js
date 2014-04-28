b1da9274dcea71720f5a4b593f7452bb5a439e29
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-28 18:52:00 +0900

    fix script.js / postリクエストのレスポンスを取得,html書き換えを関数化

--- comet/public/js/script.js
+++ comet/public/js/script.js
@@ -1,17 +1,19 @@
+function insertData(data) {
+  var json = $.parseJSON(data);
+  var lines = json.line;
+  lines.reverse();
+  for(var i=0;i<(lines.length < 10 ? line.length : 10);i++) {
+    var main = $("<div>", {class: "data"});
+    var date = $("<span>", {class: "date"}).text(lines[i]["date"]);
+    var comment = $("<div>", {class: "comment"}).text(lines[i]["name"] + " : " + lines[i]["comment"]);
+    main.append(date);
+    main.append(comment);
+    $("div#comments").append(main);
+  }
+}
+
 function getData(){
-  $.get("/comments", function(data){
-    var json = $.parseJSON(data);
-    var lines = json.line;
-    lines.reverse();
-    for(var i=0;i<(lines.length < 10 ? line.length : 10);i++){
-      var main = $("<div>", {class: "data"});
-      var date = $("<span>", {class: "date"}).text(lines[i]["date"]);
-      var comment = $("<div>", {class: "comment"}).text(lines[i]["name"] + " : " + lines[i]["comment"]);
-      main.append(date);
-      main.append(comment);
-      $("div#comments").append(main);
-    }
-  });
+  $.get("/comments", insertData(data));
     /*
   $.ajax({
     url : "/comments",
@@ -48,7 +50,7 @@ $(function() {
       alert("無言はなしですよ");
       return;
     }
-    $.post("/post", {"name": name, "comment": comment});
+    $.post("/post", {"name": name, "comment": comment}, insertData(data));
     $("input,text").not('input[type=\"button\"],input[id=\"name\"]').val("");
     return false;
   });
